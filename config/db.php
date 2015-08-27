e14473f79a2494cb4ef7d528ad5e0a570480f48b
System Administrator <root@MacBook-de-Itamar.local>
2015-08-27 00:04:02 -0700

    First Commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:8889;dbname=cenaces',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
---
+++ models/Config.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "config".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $created_at
+ */
+class Config extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'config';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['created_at'], 'safe'],
+            [['name'], 'string', 'max' => 225]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'created_at' => Yii::t('app', 'Created At'),
+        ];
+    }
+}
